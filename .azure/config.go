d733751a3438889907854ae3999258e1630857af
Josh Duffney <jduffney@microsoft.com>
2025-07-17 10:00:35 -0500

    feat: Enhance monitoring configuration with alerts and action groups

--- .azure/config.go
+++ .azure/config.go
@@ -12,6 +12,22 @@ type Config struct {
 	ResourceGroup       ResourceGroup       `json:"resourceGroup"`
 	StaticWebApp        StaticWebApp        `json:"staticWebApp"`
 	ApplicationInsights ApplicationInsights `json:"applicationInsights"`
+	MonitoringConfig    MonitoringConfig    `json:"monitoringConfig"`
+	ResourceNames       ResourceNames       `json:"resourceNames"`
+	ActionGroup         ActionGroup         `json:"actionGroup"`
+	AdminEmail          string              `json:"adminEmail"`
+	AvailabilityAlert   AvailabilityAlert   `json:"availabilityAlert"`
+	ResponseTimeAlert   ResponseTimeAlert   `json:"responseTimeAlert"`
+}
+
+type MonitoringConfig struct {
+	Frequency    int      `json:"frequency"`
+	Timeout      int      `json:"timeout"`
+	GeoLocations []string `json:"geoLocations"`
+}
+
+type ResourceNames struct {
+	WebTest string `json:"webTest"`
 }
 
 type ResourceGroup struct {
@@ -19,16 +35,43 @@ type ResourceGroup struct {
 	Location string `json:"location"`
 }
 
+type AvailabilityAlert struct {
+	Name                  string  `json:"name"`
+	Description           string  `json:"description"`
+	AvailabilityThreshold float64 `json:"availabilityThreshold"`
+	Frequency             string  `json:"frequency"`
+	WindowSize            string  `json:"windowSize"`
+	SeverityProd          int     `json:"severityProd"`
+	SeverityNonProd       int     `json:"severityNonProd"`
+}
+
+type ResponseTimeAlert struct {
+	Name                  string  `json:"name"`
+	Description           string  `json:"description"`
+	ResponseTimeThreshold float64 `json:"responseTimeThreshold"`
+	Frequency             string  `json:"frequency"`
+	WindowSize            string  `json:"windowSize"`
+	SeverityProd          int     `json:"severityProd"`
+	SeverityNonProd       int     `json:"severityNonProd"`
+}
+
+type ActionGroup struct {
+	Name      string `json:"name"`
+	ShortName string `json:"shortName"`
+}
+
 type StaticWebApp struct {
 	Name     string `json:"name"`
 	Location string `json:"location"`
 	SkuTier  string `json:"skuTier"`
+	Url      string `json:"url"`
 }
 
 type ApplicationInsights struct {
 	Name            string `json:"name"`
 	Location        string `json:"location"`
 	RetentionInDays int    `json:"retentionInDays"`
+	ApplicationType string `json:"applicationType"`
 }
 
 // GetEnvironmentConfig returns configuration based on environment variable
@@ -40,18 +83,51 @@ func GetEnvironmentConfig(environment string) Config {
 	}
 	return Config{
 		ResourceGroup: ResourceGroup{
-			Name:     fmt.Sprintf("ads-microsite-%s-rg", environment),
-			Location: "East US", // You may want to make this configurable
+			Name:     fmt.Sprintf("ads-microsite-%s-rg-cdk", environment),
+			Location: "eastus2",
 		},
 		StaticWebApp: StaticWebApp{
 			Name:     fmt.Sprintf("ads-microsite-%s-swa", environment),
-			Location: "East US", // You may want to make this configurable
-			SkuTier:  "Free",    // You may want to make this configurable
+			Location: "eastus2",
+			SkuTier:  "Free",
+			Url:      "https://ads-microsite-example.azurestaticapps.net", // Example URL
 		},
 		ApplicationInsights: ApplicationInsights{
 			Name:            fmt.Sprintf("ads-microsite-%s-ai", environment),
-			Location:        "East US", // You may want to make this configurable
-			RetentionInDays: 30,        // You may want to make this configurable
+			Location:        "eastus2",
+			RetentionInDays: 30,
+			ApplicationType: "web",
+		},
+		MonitoringConfig: MonitoringConfig{
+			Frequency:    300, // 5 minutes
+			Timeout:      30,
+			GeoLocations: []string{"us-fl-mia-edge"},
+		},
+		ResourceNames: ResourceNames{
+			WebTest: "ads-microsite-global-monitor",
+		},
+		ActionGroup: ActionGroup{
+			Name:      "ads-microsite-alerts",
+			ShortName: "alerts",
+		},
+		AdminEmail: "admin@example.com",
+		AvailabilityAlert: AvailabilityAlert{
+			Name:                  "ads-microsite-availability-alert",
+			Description:           "Alert when ads-microsite availability drops below 99%",
+			AvailabilityThreshold: 99,
+			Frequency:             "PT1M",
+			WindowSize:            "PT5M",
+			SeverityProd:          1,
+			SeverityNonProd:       2,
+		},
+		ResponseTimeAlert: ResponseTimeAlert{
+			Name:                  "ads-microsite-response-time-alert",
+			Description:           "Alert when ads-microsite response time exceeds 1000ms",
+			ResponseTimeThreshold: 1000,
+			Frequency:             "PT1M",
+			WindowSize:            "PT5M",
+			SeverityProd:          2,
+			SeverityNonProd:       3,
 		},
 		Tags: &map[string]*string{
 			"Project":   jsii.String("ads-microsite"),
--- .azure/main.go
+++ .azure/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 	"github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v14/applicationinsights"
+	"github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v14/applicationinsightswebtest"
+	"github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v14/monitoractiongroup"
+	"github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v14/monitormetricalert"
 	"github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v14/provider"
 	"github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v14/resourcegroup"
 	"github.com/cdktf/cdktf-provider-azurerm-go/azurerm/v14/staticwebapp"
@@ -14,7 +20,9 @@ func NewMyStack(scope constructs.Construct, id string, config Config) cdktf.Terr
 	stack := cdktf.NewTerraformStack(scope, &id)
 
 	provider.NewAzurermProvider(stack, jsii.String("azurerm"), &provider.AzurermProviderConfig{
-		Features: []*provider.AzurermProviderFeatures{{}},
+		Features: []*provider.AzurermProviderFeatures{{
+			// Removed unknown field prevent_deletion_if_contains_resources
+		}},
 	})
 
 	rg := resourcegroup.NewResourceGroup(stack, jsii.String("MyResourceGroup"), &resourcegroup.ResourceGroupConfig{
@@ -28,9 +36,6 @@ func NewMyStack(scope constructs.Construct, id string, config Config) cdktf.Terr
 		Name:              jsii.String(config.StaticWebApp.Name),
 		SkuTier:           jsii.String(config.StaticWebApp.SkuTier),
 		Tags:              config.Tags,
-		Identity: &staticwebapp.StaticWebAppIdentity{
-			Type: jsii.String("SystemAssigned"),
-		},
 	})
 
 	appInsights := applicationinsights.NewApplicationInsights(stack, jsii.String("MyApplicationInsights"), &applicationinsights.ApplicationInsightsConfig{
@@ -38,12 +43,114 @@ func NewMyStack(scope constructs.Construct, id string, config Config) cdktf.Terr
 		Location:          jsii.String(config.ApplicationInsights.Location),
 		Name:              jsii.String(config.ApplicationInsights.Name),
 		RetentionInDays:   jsii.Number(config.ApplicationInsights.RetentionInDays),
+		ApplicationType:   jsii.String(config.ApplicationInsights.ApplicationType),
 		Tags:              config.Tags,
+	})
+
+	applicationinsightswebtest.NewApplicationInsightsWebTest(stack, jsii.String("GlobalMonitorWebTest"), &applicationinsightswebtest.ApplicationInsightsWebTestConfig{
+		Name:                  jsii.String(config.ResourceNames.WebTest),
+		Location:              jsii.String(config.ResourceGroup.Location),
+		ResourceGroupName:     rg.Name(),
+		ApplicationInsightsId: appInsights.Id(),
+		Kind:                  jsii.String("ping"),
+		Frequency:             jsii.Number(config.MonitoringConfig.Frequency),
+		Timeout:               jsii.Number(config.MonitoringConfig.Timeout),
+		Enabled:               jsii.Bool(true),
+		GeoLocations:          jsii.Strings(config.MonitoringConfig.GeoLocations...),
+		Configuration: jsii.String(fmt.Sprintf(`
+<WebTest Name="%s" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="%d" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
+		<Items>
+				<Request Method="GET" Version="1.1" Url="%s" ThinkTime="0" Timeout="%d" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+		</Items>
+</WebTest>
+`, config.ResourceNames.WebTest, config.MonitoringConfig.Timeout, config.StaticWebApp.Url, config.MonitoringConfig.Timeout)),
+		Tags: config.Tags,
+	})
+
+	actionGroup := monitoractiongroup.NewMonitorActionGroup(stack, jsii.String("AlertsActionGroup"), &monitoractiongroup.MonitorActionGroupConfig{
+		Name:              jsii.String(config.ActionGroup.Name),
+		ResourceGroupName: rg.Name(),
+		ShortName:         jsii.String(config.ActionGroup.ShortName),
+		EmailReceiver: &[]*monitoractiongroup.MonitorActionGroupEmailReceiver{
+			{
+				Name:         jsii.String("admin"),
+				EmailAddress: jsii.String(config.AdminEmail),
+			},
+		},
+		Tags: config.Tags,
+	})
+
+	monitormetricalert.NewMonitorMetricAlert(stack, jsii.String("AvailabilityAlert"), &monitormetricalert.MonitorMetricAlertConfig{
+		Name:              jsii.String(config.AvailabilityAlert.Name),
+		ResourceGroupName: rg.Name(),
+		Scopes:            &[]*string{appInsights.Id()},
+		Description:       jsii.String(config.AvailabilityAlert.Description),
+		Criteria: &[]*monitormetricalert.MonitorMetricAlertCriteria{
+			{
+				MetricNamespace: jsii.String("Microsoft.Insights/components"),
+				MetricName:      jsii.String("availabilityResults/availabilityPercentage"),
+				Aggregation:     jsii.String("Average"),
+				Operator:        jsii.String("LessThan"),
+				Threshold:       jsii.Number(config.AvailabilityAlert.AvailabilityThreshold),
+			},
+		},
+		Action: &[]*monitormetricalert.MonitorMetricAlertAction{
+			{
+				ActionGroupId: actionGroup.Id(),
+			},
+		},
+		Frequency:  jsii.String(config.AvailabilityAlert.Frequency),
+		WindowSize: jsii.String(config.AvailabilityAlert.WindowSize),
+		Severity: jsii.Number(func() int {
+			if os.Getenv("NODE_ENV") == "prod" {
+				return config.AvailabilityAlert.SeverityProd
+			}
+			return config.AvailabilityAlert.SeverityNonProd
+		}()),
+		Tags:      config.Tags,
+		DependsOn: &[]cdktf.ITerraformDependable{actionGroup},
+	})
+
+	monitormetricalert.NewMonitorMetricAlert(stack, jsii.String("ResponseTimeAlert"), &monitormetricalert.MonitorMetricAlertConfig{
+		Name:              jsii.String(config.ResponseTimeAlert.Name),
+		ResourceGroupName: rg.Name(),
+		Scopes:            &[]*string{appInsights.Id()},
+		Description:       jsii.String(config.ResponseTimeAlert.Description),
+		Criteria: &[]*monitormetricalert.MonitorMetricAlertCriteria{
+			{
+				MetricNamespace: jsii.String("Microsoft.Insights/components"),
+				MetricName:      jsii.String("availabilityResults/duration"),
+				Aggregation:     jsii.String("Average"),
+				Operator:        jsii.String("GreaterThan"),
+				Threshold:       jsii.Number(config.ResponseTimeAlert.ResponseTimeThreshold),
+			},
+		},
+		Action: &[]*monitormetricalert.MonitorMetricAlertAction{
+			{
+				ActionGroupId: actionGroup.Id(),
+			},
+		},
+		Frequency:  jsii.String(config.ResponseTimeAlert.Frequency),
+		WindowSize: jsii.String(config.ResponseTimeAlert.WindowSize),
+		Severity: jsii.Number(func() int {
+			if os.Getenv("NODE_ENV") == "prod" {
+				return config.ResponseTimeAlert.SeverityProd
+			}
+			return config.ResponseTimeAlert.SeverityNonProd
+		}()),
+		Tags:      config.Tags,
+		DependsOn: &[]cdktf.ITerraformDependable{actionGroup},
 	})
 
 	cdktf.NewTerraformOutput(stack, jsii.String("static_web_app_name"), &cdktf.TerraformOutputConfig{
 		Value:       swa.Name(),
 		Description: jsii.String("The name of the static web app"),
+	})
+
+	// output action group ID
+	cdktf.NewTerraformOutput(stack, jsii.String("action_group_id"), &cdktf.TerraformOutputConfig{
+		Value:       actionGroup.Id(),
+		Description: jsii.String("The ID of the action group for alerts"),
 	})
 
 	return stack
